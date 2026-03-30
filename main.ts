c45f91e36a180bad9d008bf51670d44906a1801f
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2026-03-30 13:58:13 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
-    minute += 10
-    basic.showNumber(minute)
-    basic.clearScreen()
+	
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     stunde += 1
@@ -23,11 +21,23 @@ let stunde = 0
 let minute = 0
 minute = 0
 stunde = 0
+let strip = neopixel.create(DigitalPin.P3, 30, NeoPixelMode.RGB)
+strip.showRainbow(1, 360)
+strip.show()
+basic.pause(5000)
+strip.clear()
+strip.show()
+let minuten_counter = 0
 loops.everyInterval(60000, function () {
     minute += 1
     if (minute == 60) {
         minute = 0
         stunde += stunde
+    }
+    if (minute % 2 == 0 && minute != 0) {
+        strip.setPixelColor(minuten_counter, neopixel.colors(NeoPixelColors.Purple))
+        strip.show()
+        minuten_counter += 1
     }
 })
 basic.forever(function () {
