d8a9965e1330854b7d3b3b77934ea69c42badd3e
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2026-04-15 12:19:23 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,5 @@
-input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
-	
-})
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     stunde += 1
-    basic.showNumber(stunde)
-    basic.clearScreen()
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     basic.showNumber(stunde)
@@ -14,40 +9,25 @@ input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     minute += 1
-    basic.showNumber(minute)
-    basic.clearScreen()
 })
-input.onGesture(Gesture.Shake, function () {
-    for (let Index = 0; Index <= 120; Index++) {
-        strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Green))
-        strip.show()
-        basic.pause(100)
-        basic.showNumber(Index)
-    }
-})
-let strip: neopixel.Strip = null
 let stunde = 0
 let minute = 0
 minute = 0
 stunde = 0
-strip = neopixel.create(DigitalPin.P3, 30, NeoPixelMode.RGB)
-strip.showRainbow(1, 360)
-strip.show()
-basic.pause(5000)
-strip.clear()
-strip.showColor(neopixel.colors(NeoPixelColors.Blue))
-strip.show()
-let minuten_counter = 0
+let strip = neopixel.create(DigitalPin.P0, 120, NeoPixelMode.RGB)
 loops.everyInterval(60000, function () {
     minute += 1
     if (minute == 60) {
-        minute = 0
-        stunde += stunde
-    }
-    if (minute % 2 == 0 && minute != 0) {
-        strip.setPixelColor(minuten_counter, neopixel.colors(NeoPixelColors.Purple))
+        strip.clear()
         strip.show()
-        minuten_counter += 1
+        minute = 0
+        stunde += 1
+    }
+    if (minute != 0) {
+        for (let Index = 0; Index <= minute * 2; Index++) {
+            strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Red))
+        }
+        strip.show()
     }
 })
 basic.forever(function () {
