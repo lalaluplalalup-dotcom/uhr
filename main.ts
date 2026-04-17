516d21914c1d8bf95e6b348d53287a5103cfa7ce
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2026-04-17 11:08:58 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,28 +10,52 @@ input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     minute += 1
 })
-let stunde = 0
-let minute = 0
-minute = 0
-stunde = 0
-let strip = neopixel.create(DigitalPin.P0, 120, NeoPixelMode.RGB)
-strip.setBrightness(255)
-strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
-loops.everyInterval(60000, function () {
-    minute += 1
-    if (minute == 60) {
-        strip.clear()
-        strip.show()
-        minute = 0
-        stunde += 1
+function zeigeStunden () {
+    stunden_strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+    for (let Index = 0; Index <= stunde * leds_pro_stunde; Index++) {
+        stunden_strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Red))
     }
+    stunden_strip.show()
+}
+input.onGesture(Gesture.Shake, function () {
+    zeigeMinuten()
+    zeigeStunden()
+})
+function zeigeMinuten () {
     if (minute != 0) {
-        for (let Index = 0; Index <= minute * 2; Index++) {
+        for (let Index = 0; Index <= minute * leds_an_pro_minute; Index++) {
             strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Red))
         }
         strip.show()
     }
-})
-basic.forever(function () {
-	
+}
+let leds_pro_stunde = 0
+let leds_an_pro_minute = 0
+let stunden_strip: neopixel.Strip = null
+let strip: neopixel.Strip = null
+let stunde = 0
+let minute = 0
+minute = 0
+stunde = 12
+let minutenlimit = 2
+let stundenlimit = 12
+strip = neopixel.create(DigitalPin.P3, 120, NeoPixelMode.RGB)
+stunden_strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+leds_an_pro_minute = 2
+leds_pro_stunde = 3
+strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+stunden_strip.showColor(neopixel.colors(NeoPixelColors.Red))
+loops.everyInterval(60000, function () {
+    minute += 1
+    if (minute == minutenlimit) {
+        strip.clear()
+        strip.show()
+        minute = 0
+        stunde += 1
+        if (stunde > stundenlimit) {
+            stunde = 1
+        }
+    }
+    zeigeMinuten()
+    zeigeStunden()
 })
