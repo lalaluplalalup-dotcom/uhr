d57c96704603f1678f4d216beaf1dab42b467ede
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2026-04-17 11:47:08 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
+function minutenCheck () {
+    if (minute == minutenlimit) {
+        minute = 0
+        stunde += 1
+    }
+}
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     stunde += 1
+    uhrCheck()
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     basic.showNumber(stunde)
@@ -9,6 +16,7 @@ input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     minute += 1
+    uhrCheck()
 })
 function zeigeStunden () {
     stunden_strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
@@ -22,40 +30,42 @@ input.onGesture(Gesture.Shake, function () {
     zeigeStunden()
 })
 function zeigeMinuten () {
-    if (minute != 0) {
-        for (let Index = 0; Index <= minute * leds_an_pro_minute; Index++) {
-            strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Red))
-        }
-        strip.show()
+    strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
+    for (let Index = 0; Index <= minute * leds_an_pro_minute; Index++) {
+        strip.setPixelColor(Index, neopixel.colors(NeoPixelColors.Red))
+    }
+    strip.show()
+}
+function uhrCheck () {
+    minutenCheck()
+    stundenCheck()
+}
+function stundenCheck () {
+    if (stunde == stundenlimit) {
+        stunde = 0
     }
 }
 let leds_pro_stunde = 0
 let leds_an_pro_minute = 0
 let stunden_strip: neopixel.Strip = null
 let strip: neopixel.Strip = null
+let stundenlimit = 0
+let minutenlimit = 0
 let stunde = 0
 let minute = 0
 minute = 0
-stunde = 12
-let minutenlimit = 2
-let stundenlimit = 12
-strip = neopixel.create(DigitalPin.P3, 120, NeoPixelMode.RGB)
-stunden_strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+stunde = 0
+minutenlimit = 2
+stundenlimit = 12
+strip = neopixel.create(DigitalPin.P3, 118, NeoPixelMode.RGB)
+stunden_strip = neopixel.create(DigitalPin.P0, 22, NeoPixelMode.RGB)
 leds_an_pro_minute = 2
-leds_pro_stunde = 3
+leds_pro_stunde = 2
 strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
-stunden_strip.showColor(neopixel.colors(NeoPixelColors.Red))
+stunden_strip.showColor(neopixel.colors(NeoPixelColors.Indigo))
 loops.everyInterval(60000, function () {
     minute += 1
-    if (minute == minutenlimit) {
-        strip.clear()
-        strip.show()
-        minute = 0
-        stunde += 1
-        if (stunde > stundenlimit) {
-            stunde = 1
-        }
-    }
+    uhrCheck()
     zeigeMinuten()
     zeigeStunden()
 })
